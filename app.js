1148b3a29f0c95b574c029fbe24433ea06a1d8ac
cami martins <camilla@umbler.com>
2018-05-16 19:54:21 -0300

    new changes in project

--- app.js
+++ app.js
@@ -2,33 +2,37 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var clients = {}; 
+var clients = {};
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
-  });
+});
 
-io.on("connection", function (client) {  
-    client.on("join", function(name){
-    	console.log("Joined: " + name);
+app.get('/assets/css.css', function (req, res) {
+    res.sendFile(__dirname + '/assets/css.css');
+});
+
+io.on("connection", function (client) {
+    client.on("join", function (name) {
+        console.log("Entrou: " + name);
         clients[client.id] = name;
-        client.emit("update", "You have connected to the server.");
-        client.broadcast.emit("update", name + " has joined the server.")
+        client.emit("update", "Você entrou no servidor.");
+        client.broadcast.emit("update", name + " entrou no servidor.")
     });
 
-    client.on("send", function(msg){
-    	console.log("Message: " + msg);
+    client.on("send", function (msg) {
+        console.log("Mensagem: " + msg);
         client.broadcast.emit("chat", clients[client.id], msg);
     });
 
-    client.on("disconnect", function(){
-    	console.log("Disconnect");
-        io.emit("update", clients[client.id] + " has left the server.");
+    client.on("disconnect", function () {
+        console.log("Desconectou");
+        io.emit("update", clients[client.id] + " deixou o servidor.");
         delete clients[client.id];
     });
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on port 3000');
+http.listen(3000, function () {
+    console.log('ouvindo na porta 3000');
 });
